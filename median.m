f416ad5f21464646dd459722d47eb17f6b207c64
Hanimm_ay <hanimmay252@gmail.com>
2020-05-29 23:01:15 +0300

    no message

--- median.m
+++ median.m
@@ -1,7 +1,7 @@
 % Medyan Filtresi
 ResimOrjinal=imread('C:\Users\hanim\Documents\MATLAB\resimler\data1.jpg');
 ResimOrjinal=rgb2gray(ResimOrjinal);
-tuzBiber=imnoise(ResimOrjinal,'salt & pepper',0.02);  % tuz ve biber gürültüsü ekle
+tuzBiber=imnoise(ResimOrjinal,'salt & pepper',0.02);  % tuz ve biber gurultusu ekle
 medyanFiltresi=medfilt2(tuzBiber);
 figure;
 subplot(1,2,1);
